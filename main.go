1f1963ddde16badb2a11f20dcb11e17f81a1dd14
pmillerdev <pmillerdev@gmail.com>
2020-04-28 17:12:27 +0100

    variable declarations and basics of pointers

--- main.go
+++ main.go
@@ -3,5 +3,28 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello from a module, Gophers!")
+	var i int
+	i = 2
+	fmt.Println(i)
+
+	var f float32 = 3.141
+	fmt.Println(f)
+
+	name := "Bob"
+	fmt.Println(name)
+
+	b := true
+	fmt.Println(b)
+
+	c := complex(3, 4)
+	fmt.Println(c)
+
+	r, im := real(c), imag(c)
+	fmt.Println(r)
+	fmt.Println(im)
+
+	var firstName *string = new(string)
+
+	*firstName = "Arthur"
+	fmt.Println(*firstName)
 }
